b9701a8eacfba995abb6139ac61a9a84f4b82913
austinbhan <austinbhan@outlook.com>
2022-08-17 18:04:58 -0700

    Am I just stupid? Delete Test 404 Error

--- __tests__/reviews.test.js
+++ __tests__/reviews.test.js
@@ -18,7 +18,6 @@ describe('backend-express-template routes', () => {
     await agent.post('/api/v1/users').send(mockUser);
 
     const resp = await agent.post('/api/v1/restaurants/1/reviews').send({ user_id: 2, restaurant_id: 1, stars: 5, details: 'Stick Around' });
-    console.log(resp.body);
     expect(resp.status).toBe(200);
     expect(resp.body).toEqual({
       id: expect.any(String),
@@ -27,7 +26,20 @@ describe('backend-express-template routes', () => {
       stars: expect.any(String),
       details: expect.any(String)
     });
+  });
 
+  it('#DELETE. If logged in or admin, delete a restaurant review', async () => {
+    // const agent = request.agent(app);
+    // await agent.post('/api/v1/users').send({
+    //   email: 'admin',
+    //   password: '1234',
+    // });
+    // await agent
+    //   .post('/api/v1/users/sessions')
+    //   .send({ email: 'admin', password: '1234' });
+
+    const resp = await request(app).delete('/api/v1/reviews/1');
+    expect(resp.status).toBe(200);
   });
 
   afterAll(() => {
